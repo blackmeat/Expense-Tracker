059ec608bdbcb30decc96d1ecfbcb6d150b96951
Henry Wong <joe8632782@gmail.com>
2019-12-23 16:05:47 +0800

    Install passport-facebook&dotenv and Add facebook login feature and Remove fbsecret key

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 // 載入express模組
 const express = require("express")
+// 如果不是 production 模式
+if (process.env.NODE_ENV !== 'production') {
+  // 使用 dotenv 讀取 .env 檔案
+  require('dotenv').config()
+}
 const app = express()
 const port = 3000
 // 載入mongoose模組 (與資料庫連結)
@@ -63,6 +68,7 @@ app.use((req, res, next) => {
 app.use("/", require("./routes/home"))
 app.use("/records", require("./routes/record"))
 app.use("/users", require("./routes/user"))
+app.use("/auth", require("./routes/auths"))
 
 
 
